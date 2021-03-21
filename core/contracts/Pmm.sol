27535c821d58a46c5568e12ac2a10f54322cbd74
Pisuth D <pisuth.dae@gmail.com>
2021-03-21 10:36:05 +0700

    Feat: Complete PMM trade functions

--- core/contracts/Pmm.sol
+++ core/contracts/Pmm.sol
@@ -10,6 +10,7 @@ import "./utility/LibMath.sol";
 import "./utility/SafeERC20.sol";
 import "./interfaces/IPmm.sol";
 import "./TokenFactory.sol";
+import "./utility/DODOMath.sol";
 
 /**
  * @title PMM contract
@@ -20,6 +21,7 @@ contract Pmm is Lockable, Whitelist, IPmm {
     using LibMathUnsigned for uint256;
     using SafeERC20 for IERC20;
 
+    enum RStatus {ONE, ABOVE_ONE, BELOW_ONE}
     
     // Price feeder contract.
     IPriceFeeder public priceFeeder;
@@ -32,20 +34,32 @@ contract Pmm is Lockable, Whitelist, IPmm {
     IExpandedIERC20 public override quoteCapitalToken;
     // Variables for PMM Algorithm
     uint256 public k;
-    uint256 public lpFeeRate;
-    uint256 public mtFeeRate;
-    
+    RStatus public rStatus; // reverse status of K
+    uint256 public targetBaseTokenAmount;
+    uint256 public targetQuoteTokenAmount;
+    uint256 public baseBalance;
+    uint256 public quoteBalance;
 
+    uint256 constant ONE = 1000000000000000000;
 
     event CreatedPMM();
+
+    event Deposit(
+        address indexed payer,
+        address indexed receiver,
+        bool isBaseToken,
+        uint256 amount,
+        uint256 lpTokenAmount
+    );
+
+    event BuyBaseToken(address indexed buyer, uint256 receiveBase, uint256 payQuote);
+    event SellBaseToken(address indexed seller, uint256 payBase, uint256 receiveQuote);
     
     constructor(
         address _tokenFactoryAddress,
         address _baseToken,
         address _quoteToken,
         address _priceFeeder,
-        uint256 _lpFeeRate,
-        uint256 _mtFeeRate,
         uint256 _k
     ) public nonReentrant() {
         require(address(_priceFeeder) != address(0), "Invalid PriceFeeder address");
@@ -62,8 +76,7 @@ contract Pmm is Lockable, Whitelist, IPmm {
         quoteCapitalToken = tf.createToken("QUOTE_TLP", "TLP", 18);
 
         k = _k;
-        lpFeeRate = _lpFeeRate;
-        mtFeeRate = _mtFeeRate;
+        rStatus = RStatus.ONE;
 
         addAddress(msg.sender);
         emit CreatedPMM();
@@ -71,14 +84,438 @@ contract Pmm is Lockable, Whitelist, IPmm {
         _valiateParameters();
     }
 
+    // Deposit base token
+    function depositBase(uint256 amount) external returns (uint256) {
+        return depositBaseTo(msg.sender, amount);
+    }
 
+    // Deposit quote token
+    function depositQuote(uint256 amount) external returns (uint256) {
+        return depositQuoteTo(msg.sender, amount);
+    }
 
+    // Total supply of baseCapitalToken  
+    function getTotalBaseCapital() public view returns (uint256) {
+        return baseCapitalToken.totalSupply();
+    }
 
-    // INTERNAL FUCTIONS
+    // Total supply of quoteCapitalToken
+    function getTotalQuoteCapital() public view returns (uint256) {
+        return quoteCapitalToken.totalSupply();
+    }
+
+    function depositQuoteTo(address to, uint256 amount)
+        public
+        nonReentrant()
+        returns (uint256)
+    {
+        (, uint256 quoteTarget) = getExpectedTarget();
+        uint256 totalQuoteCapital = getTotalQuoteCapital();
+        uint256 capital = amount;
+        if (totalQuoteCapital == 0) {
+            // give remaining quote token to lp as a gift
+            capital = amount.add(quoteTarget);
+        } else if (quoteTarget > 0) {
+            capital = amount.wmul(totalQuoteCapital).wdiv(quoteTarget);
+        }
+
+        // settlement
+        _quoteTokenTransferIn(msg.sender, amount);
+        _mintQuoteCapital(to, capital);
+        targetQuoteTokenAmount = targetQuoteTokenAmount.add(amount);
+
+        emit Deposit(msg.sender, to, false, amount, capital);
+        return capital;
+    }
+
+    function depositBaseTo(address to, uint256 amount)
+        public
+        nonReentrant()
+        returns (uint256)
+    {
+        (uint256 baseTarget, ) = getExpectedTarget();
+        uint256 totalBaseCapital = getTotalBaseCapital();
+        uint256 capital = amount;
+        if (totalBaseCapital == 0) {
+            // give remaining base token to lp as a gift
+            capital = amount.add(baseTarget);
+        } else if (baseTarget > 0) {
+            capital = amount.wmul(totalBaseCapital).wdiv(baseTarget);
+        }
+
+        // settlement
+        _baseTokenTransferIn(msg.sender, amount);
+        _mintBaseCapital(to, capital);
+        targetBaseTokenAmount = targetBaseTokenAmount.add(amount);
+
+        emit Deposit(msg.sender, to, true, amount, capital);
+        return capital;
+    }
+
+    // calculate quote token -> base token
+    function queryBuyBaseToken(uint256 amount) external view returns (uint256 payQuote) {
+        (payQuote,  , , ) = _queryBuyBaseToken(amount);
+        return payQuote;
+    }
+
+    // calculate base token -> quote token
+    function querySellBaseToken(uint256 amount) external view returns (uint256 receiveQuote) {
+        (receiveQuote,  , , ) = _querySellBaseToken(amount);
+        return receiveQuote;
+    }
+
+    // Buy base token
+    function buyBaseToken(uint256 amount, uint256 maxPayQuote)
+        external
+        nonReentrant() 
+        returns (uint256)
+    {
+        // query price
+        (
+            uint256 payQuote,
+            RStatus newRStatus,
+            uint256 newQuoteTarget,
+            uint256 newBaseTarget
+        ) = _queryBuyBaseToken(amount);
+        require(payQuote <= maxPayQuote, "Exceeding maximum limit");
+
+        // settle assets
+        _baseTokenTransferOut(msg.sender, amount);
+        _quoteTokenTransferIn(msg.sender, payQuote);
+
+        // update TARGET
+        if (targetQuoteTokenAmount != newQuoteTarget) {
+            targetQuoteTokenAmount = newQuoteTarget;
+        }
+        if (targetBaseTokenAmount != newBaseTarget) {
+            targetBaseTokenAmount = newBaseTarget;
+        }
+        if (rStatus != newRStatus) {
+            rStatus = newRStatus;
+        }
+
+        emit BuyBaseToken(msg.sender, amount, payQuote);
+
+        return payQuote;
+    }
+
+    // Sell base token
+    function sellBaseToken(
+        uint256 amount,
+        uint256 minReceiveQuote
+    ) external nonReentrant() returns (uint256) {
+        // query price
+        (
+            uint256 receiveQuote,
+            RStatus newRStatus,
+            uint256 newQuoteTarget,
+            uint256 newBaseTarget
+        ) = _querySellBaseToken(amount);
+        require(receiveQuote >= minReceiveQuote, "SELL_BASE_RECEIVE_NOT_ENOUGH");
+
+        // settle assets
+        _quoteTokenTransferOut(msg.sender, receiveQuote);
+        _baseTokenTransferIn(msg.sender, amount);
+
+        // update TARGET
+        if (targetQuoteTokenAmount != newQuoteTarget) {
+            targetQuoteTokenAmount = newQuoteTarget;
+        }
+        if (targetBaseTokenAmount != newBaseTarget) {
+            targetBaseTokenAmount = newBaseTarget;
+        }
+        if (rStatus != newRStatus) {
+            rStatus = newRStatus;
+        }
+
+        emit SellBaseToken(msg.sender, amount, receiveQuote);
+
+        return receiveQuote;
+    }
+
+    function getMidPrice() public view returns (uint256 midPrice) {
+        (uint256 baseTarget, uint256 quoteTarget) = getExpectedTarget();
+        if (rStatus == RStatus.BELOW_ONE) {
+            uint256 R = (quoteTarget.mul(quoteTarget).div(quoteBalance)).wdiv(quoteBalance);
+            R = ONE.sub(k).add(k.wmul(R));
+            return (priceFeeder.getValue()).wdiv(R);
+        } else {
+            uint256 R = (baseTarget.mul(baseTarget).div(baseBalance).wdiv(baseBalance));
+            R = ONE.sub(k).add(k.wmul(R));
+            return (priceFeeder.getValue()).wmul(R);
+        }
+    }
+
+    function getExpectedTarget() public view returns (uint256 baseTarget, uint256 quoteTarget) {
+        uint256 Q = quoteBalance;
+        uint256 B = baseBalance;
+        if (rStatus == RStatus.ONE) {
+            return (targetBaseTokenAmount, targetQuoteTokenAmount);
+        } else if (rStatus == RStatus.BELOW_ONE) {
+            uint256 payQuoteToken = _RBelowBackToOne();
+            return (targetBaseTokenAmount, Q.add(payQuoteToken));
+        } else if (rStatus == RStatus.ABOVE_ONE) {
+            uint256 payBaseToken = _RAboveBackToOne();
+            return (B.add(payBaseToken), targetQuoteTokenAmount);
+        }
+    }
+
+    // // INTERNAL FUCTIONS
     function _valiateParameters() internal view returns (uint256) {
-        require(k < 1000000000000000000, "K>=1");
+        require(k < ONE, "K>=1");
         require(k > 0, "K=0");
-        require(lpFeeRate.add(mtFeeRate) < 1000000000000000000, "FEE_RATE>=1");
+    }
+
+    function _baseTokenTransferIn(address from, uint256 amount) internal {
+        baseToken.transferFrom(from, address(this), amount);
+        baseBalance = baseBalance.add(amount);
+    }
+
+    function _quoteTokenTransferIn(address from, uint256 amount) internal {
+        quoteToken.transferFrom(from, address(this), amount);
+        quoteBalance = quoteBalance.add(amount);
+    }
+
+    function _baseTokenTransferOut(address to, uint256 amount) internal {
+        baseToken.transfer(to, amount);
+        baseBalance = baseBalance.sub(amount);
+    }
+
+    function _quoteTokenTransferOut(address to, uint256 amount) internal {
+       quoteToken.transfer(to, amount);
+       quoteBalance = quoteBalance.sub(amount);
+    }
+
+    function _mintBaseCapital(address user, uint256 amount) internal {
+        baseCapitalToken.mint(user, amount);
+    }
+
+    function _mintQuoteCapital(address user, uint256 amount) internal {
+        quoteCapitalToken.mint(user, amount);
+    }
+
+    function _queryBuyBaseToken(uint256 amount)
+        internal
+        view
+        returns (
+            uint256 payQuote,
+            RStatus newRStatus,
+            uint256 newQuoteTarget,
+            uint256 newBaseTarget
+        )
+    {
+        (newBaseTarget, newQuoteTarget) = getExpectedTarget();
+
+        uint256 buyBaseAmount = amount;
+
+        if (rStatus == RStatus.ONE) {
+            // case 1: R=1
+            payQuote = _ROneBuyBaseToken(buyBaseAmount, newBaseTarget);
+            newRStatus = RStatus.ABOVE_ONE;
+        } else if (rStatus == RStatus.ABOVE_ONE) {
+            // case 2: R>1
+            payQuote = _RAboveBuyBaseToken(buyBaseAmount, baseBalance, newBaseTarget);
+            newRStatus = RStatus.ABOVE_ONE;
+        } else if (rStatus == RStatus.BELOW_ONE) {
+            uint256 backToOnePayQuote = newQuoteTarget.sub(quoteBalance);
+            uint256 backToOneReceiveBase = baseBalance.sub(newBaseTarget);
+            // case 3: R<1
+            // complex case, R status may change
+            if (buyBaseAmount < backToOneReceiveBase) {
+                // case 3.1: R status do not change
+                // no need to check payQuote because spare base token must be greater than zero
+                payQuote = _RBelowBuyBaseToken(buyBaseAmount, quoteBalance, newQuoteTarget);
+                newRStatus = RStatus.BELOW_ONE;
+            } else if (buyBaseAmount == backToOneReceiveBase) {
+                // case 3.2: R status changes to ONE
+                payQuote = backToOnePayQuote;
+                newRStatus = RStatus.ONE;
+            } else {
+                // case 3.3: R status changes to ABOVE_ONE
+                payQuote = backToOnePayQuote.add(
+                    _ROneBuyBaseToken(buyBaseAmount.sub(backToOneReceiveBase), newBaseTarget)
+                );
+                newRStatus = RStatus.ABOVE_ONE;
+            }
+        }
+
+        return (payQuote, newRStatus, newQuoteTarget, newBaseTarget);
+    }
+
+    function _querySellBaseToken(uint256 amount)
+        internal
+        view
+        returns (
+            uint256 receiveQuote,
+            RStatus newRStatus,
+            uint256 newQuoteTarget,
+            uint256 newBaseTarget
+        )
+    {
+        (newBaseTarget, newQuoteTarget) = getExpectedTarget();
+
+        uint256 sellBaseAmount = amount;
+
+        if (rStatus == RStatus.ONE) {
+            // case 1: R=1
+            // R falls below one
+            receiveQuote = _ROneSellBaseToken(sellBaseAmount, newQuoteTarget);
+            newRStatus = RStatus.BELOW_ONE;
+        } else if (rStatus == RStatus.ABOVE_ONE) {
+            uint256 backToOnePayBase = newBaseTarget.sub(baseBalance);
+            uint256 backToOneReceiveQuote = quoteBalance.sub(newQuoteTarget);
+            // case 2: R>1
+            // complex case, R status depends on trading amount
+            if (sellBaseAmount < backToOnePayBase) {
+                // case 2.1: R status do not change
+                receiveQuote = _RAboveSellBaseToken(sellBaseAmount, baseBalance, newBaseTarget);
+                newRStatus = RStatus.ABOVE_ONE;
+                if (receiveQuote > backToOneReceiveQuote) {
+                    // [Important corner case!] may enter this branch when some precision problem happens. And consequently contribute to negative spare quote amount
+                    // to make sure spare quote>=0, mannually set receiveQuote=backToOneReceiveQuote
+                    receiveQuote = backToOneReceiveQuote;
+                }
+            } else if (sellBaseAmount == backToOnePayBase) {
+                // case 2.2: R status changes to ONE
+                receiveQuote = backToOneReceiveQuote;
+                newRStatus = RStatus.ONE;
+            } else {
+                // case 2.3: R status changes to BELOW_ONE
+                receiveQuote = backToOneReceiveQuote.add(
+                    _ROneSellBaseToken(sellBaseAmount.sub(backToOnePayBase), newQuoteTarget)
+                );
+                newRStatus = RStatus.BELOW_ONE;
+            }
+        } else {
+            // _R_STATUS_ == Types.RStatus.BELOW_ONE
+            // case 3: R<1
+            receiveQuote = _RBelowSellBaseToken(sellBaseAmount, quoteBalance, newQuoteTarget);
+            newRStatus = RStatus.BELOW_ONE;
+        }
+
+        return (receiveQuote, newRStatus, newQuoteTarget, newBaseTarget);
+    }
+
+    // DODO Pricing Model
+    function _ROneSellBaseToken(uint256 _amount, uint256 _targetQuoteTokenAmount)
+        internal
+        view
+        returns (uint256 receiveQuoteToken)
+    {
+        uint256 i = priceFeeder.getValue();
+        uint256 Q2 = DODOMath._SolveQuadraticFunctionForTrade(
+            _targetQuoteTokenAmount,
+            _targetQuoteTokenAmount,
+            i.wmul(_amount),
+            false,
+            k
+        );
+        // in theory Q2 <= targetQuoteTokenAmount
+        // however when amount is close to 0, precision problems may cause Q2 > targetQuoteTokenAmount
+        return _targetQuoteTokenAmount.sub(Q2);
+    }
+
+    function _ROneBuyBaseToken(uint256 _amount, uint256 _targetBaseTokenAmount)
+        internal
+        view
+        returns (uint256 payQuoteToken)
+    {
+        require(_amount < _targetBaseTokenAmount, "DODO_BASE_BALANCE_NOT_ENOUGH");
+        uint256 B2 = _targetBaseTokenAmount.sub(_amount);
+        payQuoteToken = _RAboveIntegrate(_targetBaseTokenAmount, _targetBaseTokenAmount, B2);
+        return payQuoteToken;
+    }
+
+    function _RBelowSellBaseToken(
+        uint256 _amount,
+        uint256 _quoteBalance,
+        uint256 _targetQuoteAmount
+    ) internal view returns (uint256 receiveQuoteToken) {
+        uint256 i = priceFeeder.getValue();
+        uint256 Q2 = DODOMath._SolveQuadraticFunctionForTrade(
+            _targetQuoteAmount,
+            _quoteBalance,
+            i.wmul(_amount),
+            false,
+            k
+        );
+        return quoteBalance.sub(Q2);
+    }
+
+    function _RBelowBuyBaseToken(
+        uint256 _amount,
+        uint256 _quoteBalance,
+        uint256 _targetQuoteAmount
+    ) internal view returns (uint256 payQuoteToken) {
+        // Here we don't require amount less than some value
+        // Because it is limited at upper function
+        // See Trader.queryBuyBaseToken
+        uint256 i = priceFeeder.getValue();
+        uint256 Q2 = DODOMath._SolveQuadraticFunctionForTrade(
+            _targetQuoteAmount,
+            _quoteBalance,
+            i.wmul(_amount),
+            true,
+            k
+        );
+        return Q2.sub(_quoteBalance);
+    }
+
+    function _RBelowBackToOne() internal view returns (uint256 payQuoteToken) {
+        // important: carefully design the system to make sure spareBase always greater than or equal to 0
+        uint256 spareBase = baseBalance.sub(targetBaseTokenAmount);
+        uint256 price = priceFeeder.getValue();
+        uint256 fairAmount = spareBase.wmul(price);
+        uint256 newTargetQuote = DODOMath._SolveQuadraticFunctionForTarget(
+            quoteBalance,
+            k,
+            fairAmount
+        );
+        return newTargetQuote.sub(quoteBalance);
+    }
+
+    function _RAboveBuyBaseToken(
+        uint256 _amount,
+        uint256 _baseBalance,
+        uint256 _targetBaseAmount
+    ) internal view returns (uint256 payQuoteToken) {
+        require(_amount < _baseBalance, "DODO_BASE_BALANCE_NOT_ENOUGH");
+        uint256 B2 = _baseBalance.sub(_amount);
+        return _RAboveIntegrate(_targetBaseAmount, _baseBalance, B2);
+    }
+
+    function _RAboveSellBaseToken(
+        uint256 _amount,
+        uint256 _baseBalance,
+        uint256 _targetBaseAmount
+    ) internal view returns (uint256 receiveQuoteToken) {
+        // here we don't require B1 <= targetBaseAmount
+        // Because it is limited at upper function
+        // See Trader.querySellBaseToken
+        uint256 B1 = _baseBalance.add(_amount);
+        return _RAboveIntegrate(_targetBaseAmount, B1, _baseBalance);
+    }
+
+    function _RAboveBackToOne() internal view returns (uint256 payBaseToken) {
+        // important: carefully design the system to make sure spareBase always greater than or equal to 0
+        uint256 spareQuote = quoteBalance.sub(targetQuoteTokenAmount);
+        uint256 price = priceFeeder.getValue();
+        uint256 fairAmount = spareQuote.wdiv(price);
+        uint256 newTargetBase = DODOMath._SolveQuadraticFunctionForTarget(
+            baseBalance,
+            k,
+            fairAmount
+        );
+        return newTargetBase.sub(baseBalance);
+    }
+
+    function _RAboveIntegrate(
+        uint256 B0,
+        uint256 B1,
+        uint256 B2
+    ) internal view returns (uint256) {
+        uint256 i = priceFeeder.getValue();
+        return DODOMath._GeneralIntegrate(B0, B1, B2, i, k);
     }
 
 }
--- core/test/Pmm.js
+++ core/test/Pmm.js
@@ -2,11 +2,14 @@ const Pmm = artifacts.require('Pmm');
 const MockToken = artifacts.require('MockToken')
 const PriceFeeder = artifacts.require('PriceFeeder')
 const TokenFactory = artifacts.require('TokenFactory')
+const SyntheticToken = artifacts.require('SyntheticToken')
 
 let priceFeed
 let baseToken
 let quoteToken
 let pmm
+let baseCapitalToken
+let quoteCapitalToken
 
 contract('Pmm', accounts => {
 
@@ -19,6 +22,14 @@ contract('Pmm', accounts => {
         baseToken = await MockToken.new("Test Synthetic", "TEST")
         quoteToken = await MockToken.new("USD Stablecoin", "USD")
         priceFeed  = await PriceFeeder.new("TEST/USD")
+
+        await baseToken.transfer(alice, web3.utils.toWei("100"))
+        await baseToken.transfer(bob, web3.utils.toWei("100"))
+        await quoteToken.transfer(alice, web3.utils.toWei("100000"))
+        await quoteToken.transfer(bob, web3.utils.toWei("100000"))
+
+        await priceFeed.updateValue(web3.utils.toWei("100"))
+
         tokenFactory = await TokenFactory.new()
 
         pmm = await Pmm.new(
@@ -26,15 +37,97 @@ contract('Pmm', accounts => {
             baseToken.address,
             quoteToken.address,
             priceFeed.address,
-            web3.utils.toWei("0.002"), // LP Fee Rate 0.2%
-            web3.utils.toWei("0.001"), // Maintenance Fee Rate 0.1%
-            web3.utils.toWei("0.1") // K 
+            web3.utils.toWei("0.99") // K 
         )
 
+        const baseCapitalTokenAddress = await pmm.baseCapitalToken()
+        const quoteCapitalTokenAddress = await pmm.quoteCapitalToken()
+
+        baseCapitalToken = await SyntheticToken.at(baseCapitalTokenAddress)
+        quoteCapitalToken = await SyntheticToken.at(quoteCapitalTokenAddress)
+
     });
 
-    it('test test test', async () => {
-        assert.equal( true, true);
+    it('Buy base token from PMM', async () => {
+        // Deposit 100 TEST to PMM
+        await baseToken.approve( pmm.address , web3.utils.toWei("1000000"))
+        await pmm.depositBase(web3.utils.toWei("100"))
+        // Deposit 10000 USD to PMM
+        await quoteToken.approve( pmm.address , web3.utils.toWei("1000000"))
+        await pmm.depositQuote(web3.utils.toWei("10000"))
+
+        const aliceOriginalBalance = await quoteToken.balanceOf(alice)
+
+        // Buy 5 TEST
+        await quoteToken.approve( pmm.address , web3.utils.toWei("100000") , { from : alice })
+        await pmm.buyBaseToken(web3.utils.toWei("5"), web3.utils.toWei("100000") , { from : alice })
+        const aliceBalanceFirstTrade = await quoteToken.balanceOf(alice)
+        assert(Number(web3.utils.fromWei(aliceOriginalBalance)) - Number(web3.utils.fromWei(aliceBalanceFirstTrade)), 526.0526315789466) // avg price = 105.2
+
+        // Buy another 5 TEST
+        await pmm.buyBaseToken(web3.utils.toWei("5"), web3.utils.toWei("100000") , { from : alice })
+        const aliceBalanceSecondTrade = await quoteToken.balanceOf(alice)
+        assert(Number(web3.utils.fromWei(aliceBalanceFirstTrade)) - Number(web3.utils.fromWei(aliceBalanceSecondTrade)), 583.9473684210534 ) // avg price = 116.78
+    })
+
+    it('Sell base token from PMM', async () => {
+      
+        // Deposit 100 TEST to PMM
+        await baseToken.approve( pmm.address , web3.utils.toWei("1000000"))
+        await pmm.depositBase(web3.utils.toWei("100"))
+        // Deposit 10000 USD to PMM
+        await quoteToken.approve( pmm.address , web3.utils.toWei("1000000"))
+        await pmm.depositQuote(web3.utils.toWei("10000"))
+
+        const aliceOriginalBalance = await quoteToken.balanceOf(alice)
+
+        // Sell 1 TEST
+        await baseToken.approve( pmm.address , web3.utils.toWei("100000") , { from : alice })
+        await pmm.sellBaseToken(web3.utils.toWei("1"), web3.utils.toWei("0") , { from : alice })
+        const aliceBalanceFirstTrade = await quoteToken.balanceOf(alice)
+
+        assert(Number(web3.utils.fromWei(aliceBalanceFirstTrade)) - Number(web3.utils.fromWei(aliceOriginalBalance)), 99.01960879497614865) // avg price 99.01
+
+        // Sell another 5 TEST
+        await pmm.sellBaseToken(web3.utils.toWei("5"), web3.utils.toWei("0") , { from : alice })
+        const aliceBalanceSecondTrade = await quoteToken.balanceOf(alice)
+
+        assert(Number(web3.utils.fromWei(aliceBalanceSecondTrade)) - Number(web3.utils.fromWei(aliceBalanceFirstTrade)), 467.32071324311255) // avg price 93.4
+
+    })
+
+    it('Buy base token from one-side funded PMM', async () => {
+        // Set TEST/USD to 10
+        await priceFeed.updateValue(web3.utils.toWei("10"))
+
+        // Deposit 10000 TEST to PMM
+        await baseToken.approve( pmm.address , web3.utils.toWei("1000000"))
+        await pmm.depositBase(web3.utils.toWei("10000"))
+
+        const aliceOriginalBalance = await quoteToken.balanceOf(alice)
+        assert(web3.utils.fromWei(aliceOriginalBalance) , "100000")
+
+        // 10% depth
+        await quoteToken.approve( pmm.address , web3.utils.toWei("100000") , { from : alice })
+        await pmm.buyBaseToken(web3.utils.toWei("1000"), web3.utils.toWei("100000") , { from : alice })
+        const aliceBalanceFirstTrade = await quoteToken.balanceOf(alice)
+        assert( web3.utils.fromWei(await baseToken.balanceOf(alice)) , "1000"  )
+        assert(Number(web3.utils.fromWei(aliceOriginalBalance)) - Number(web3.utils.fromWei(aliceBalanceFirstTrade)), 11100) // avg price = 11.1
+
+        // 20% depth
+        await pmm.buyBaseToken(web3.utils.toWei("1000"), web3.utils.toWei("100000") , { from : alice })
+        const aliceBalanceSecondTrade = await quoteToken.balanceOf(alice)
+        assert( web3.utils.fromWei(await baseToken.balanceOf(alice)) , "2000"  )
+        assert(Number(web3.utils.fromWei(aliceBalanceFirstTrade)) - Number(web3.utils.fromWei(aliceBalanceSecondTrade)), 13850) // avg price 13.85
+
+        // 50% depth
+        await pmm.buyBaseToken(web3.utils.toWei("3000"), web3.utils.toWei("100000") , { from : alice })
+        const aliceBalanceThirdTrade = await quoteToken.balanceOf(alice)
+        assert( web3.utils.fromWei(await baseToken.balanceOf(alice)) , "5000"  ) 
+        assert(Number(web3.utils.fromWei(aliceBalanceSecondTrade)) - Number(web3.utils.fromWei(aliceBalanceThirdTrade)), 74550) // avg price 24.85
+
     });
+
+    
 
 })
