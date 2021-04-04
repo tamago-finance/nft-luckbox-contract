e9a26adeb51e9ae5843a669969039b69d91aed9e
Pisuth D <pisuth.dae@gmail.com>
2021-04-04 20:12:56 +0700

    Feat: Update landing pages

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,15 +1,26 @@
 import React, { useState, useCallback } from 'react'
-import { Container, Button } from "reactstrap"
+import { Container, Button, Alert, Row, Col } from "reactstrap"
 import styled from "styled-components"
 import TokenList from "./TokenList"
-
-// the first vAMM based perpetual contract protocol built on #Neo.
-
-// Why Tamago Finance?
-
-// How to get started
+import IllustrationPNG from "../../assets/img/illustration-1.png"
+import IllustrationPNG2 from "../../assets/img/illustration-2.png"
 
 // Join Our Community
+
+// Roadmap
+
+const Illustration = styled.img.attrs(props => ({
+    src: IllustrationPNG
+}))`
+    width: 80%;
+`
+
+const Illustration2 = styled.img.attrs(props => ({
+    src: IllustrationPNG2
+}))`
+    width: 100%; 
+`
+
 
 
 const Headline = styled(
@@ -18,9 +29,6 @@ const Headline = styled(
             <div className={className}>
                 <div>
                     <h3>The first PMM based perpetual swap protocol built for non-crypto derivatives</h3>
-                    {/* <p>
-                        HotpotSwap allows trade any major stock market indices up to 10x leverage via perpetual swap contracts using BUSD as a collateral on Binance Smart Chain.
-                    </p> */}
                 </div>
                 <div className="button-row">
                     {/* <Button onClick={() => setNetwork(0)} color={currentNetwork === 0 ? "info" : "secondary"}>
@@ -37,8 +45,7 @@ const Headline = styled(
         )
     })`
     text-align: center; 
-    padding-top: 30px;
-    padding-bottom: 10px;
+    padding-top: 30px; 
 
     .button-row {
         margin-top: 20px;
@@ -61,6 +68,74 @@ const Headline = styled(
     }
     `
 
+const About = styled(
+    ({ className }) => (
+        <div className={className}>
+            <Row>
+                <Col lg="6">
+                    <Button style={{ cursor: "default" }} color="warning" />
+                    <h2>Trade all asset classes up to 4x leverage</h2>
+                    <p>
+                        Tamago Finance is a derivatives DEX where any asset classes are possible to be listed through our unique price discovery mechanism
+                    </p>
+                </Col>
+                <Col lg="6">
+                    <div style={{ textAlign: "center" }}>
+                        <Illustration />
+                    </div>
+                </Col>
+                <Col lg="6">
+                    <div style={{ textAlign: "center" }}>
+                        <Illustration2/>
+                    </div>
+                </Col>
+                <Col lg="6">
+                    <Button style={{ cursor: "default" }} color="warning" />
+                    <h2>Combine the best features of Synthetix and DODO</h2>
+                    <p>
+                        We're applying DODO’s PMM model that helps the synthetic derivatives asset capable of proactively adjusting prices from Oracle or on-chain states
+                    </p>
+                </Col>
+            </Row>
+        </div>
+    )
+)`
+        margin-top: 40px;
+        margin-bottom: 40px;
+
+        h2 {
+            font-weight: 600;
+            font-size: 24px;
+            letter-spacing: -1px;
+            margin-bottom: 20px;
+            margin-top: 10px;
+        }
+
+    `
+
+
+const Footer = styled(
+    ({ className }) => (
+        <div className={className}>
+            <div>
+                Buidl during Chainlink Hackathon 2021
+            </div>
+            <div>
+                © Tamago Finance, 2021. All rights reserved.
+            </div>
+        </div>
+    ))`
+    margin-top: 20px;
+    display: flex;
+    flex-direction: row;
+    font-size: 12px;
+    div {
+        flex: 50%;
+        :last-child {
+            text-align: right;
+        }
+    }
+    `
 
 const Home = () => {
 
@@ -72,9 +147,14 @@ const Home = () => {
                 currentNetwork={currentNetwork}
                 setNetwork={setNetwork}
             />
+            <Alert style={{ marginBottom: 20, textAlign: "center" }} color="warning">
+                Please note that the project is under heavy development and available only on Testnet
+            </Alert>
             <TokenList
                 currentNetwork={currentNetwork}
             />
+            <About />
+            <Footer />
         </Container>
     )
 }
