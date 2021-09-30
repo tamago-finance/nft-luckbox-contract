a54395d6b72c1c49259222588ad153a279bd67cb
pisuthd <pisuth.dae@gmail.com>
2021-09-30 20:03:29 +0700

    refactor: more info / helper functions

--- contractV2/contracts/utility/Timelock.sol
+++ contractV2/contracts/utility/Timelock.sol
@@ -1,10 +1,13 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "../interfaces/IERC20.sol";
 import "./SafeERC20.sol";
+import "./Lockable.sol";
 
-contract Timelock is Ownable {
+contract Timelock is Ownable, Lockable {
 
 	using SafeERC20 for IERC20;
 
@@ -18,14 +21,36 @@ contract Timelock is Ownable {
 	Batch[] public batchesInfo;	
 
 	event Deposit(address indexed account, uint256 amount, uint256 unlockBlock);
-	event Withdraw(address indexed account, uint256 amount);
+	event Withdraw(address indexed account, uint256 amount, uint256 batchId);
 
-	constructor(address _lockToken) public {
+	// must provide the token address
+	constructor(address _lockToken) public nonReentrant() {
 		lockToken = _lockToken;
 	}
 
-	function deposit(uint256 _amount, uint256 _unlockBlock) public onlyOwner {
-		require(block.number < _unlockBlock, "UnlockBlock must more than block number");
+	// return total batch in the system
+	function totalBatch() public view returns (uint256) {
+		return batchesInfo.length;
+	}
+
+	// return the batch's block to be released
+	function getBatchUnlockBlock(uint256 _batchId) public view returns (uint256) {
+		return batchesInfo[_batchId].unlockBlock;
+	}
+
+	// return the batch's amount
+	function getBatchAmount(uint256 _batchId) public view returns (uint256) {
+		return batchesInfo[_batchId].amount;
+	}
+
+	// check the withdraw status
+	function getBatchWithdrawStatus(uint256 _batchId) public view returns (bool) {
+		return batchesInfo[_batchId].canWithdraw;
+	}
+
+	// deposit the token until reach the given _unlockBlock
+	function deposit(uint256 _amount, uint256 _unlockBlock) public onlyOwner nonReentrant {
+		require(block.number < _unlockBlock, "Invalid given unlockBlock");
 		IERC20(lockToken).safeTransferFrom(msg.sender, address(this), _amount);
 		batchesInfo.push(
 			Batch(_amount, _unlockBlock, true)
@@ -33,10 +58,11 @@ contract Timelock is Ownable {
 		emit Deposit(msg.sender, _amount, _unlockBlock);
 	}
 
-	function withdraw(uint256 _batchId) public onlyOwner {
-		require(_batchId < batchesInfo.length, "Over range of batches");
+	// withdraw the token if the unlockBlock is reached 
+	function withdraw(uint256 _batchId) public onlyOwner nonReentrant {
+		require(_batchId < batchesInfo.length, "Invalid given batchId");
 		Batch storage batch = batchesInfo[_batchId];
-		require(batch.canWithdraw, "This batchId is withdraw already");
+		require(batch.canWithdraw, "Withdraw status is locked");
 		uint256 amount = batch.amount;
 		uint256 unlockBlock = batch.unlockBlock;
 		require(block.number >= unlockBlock, "You can not withdraw before unlockBlock");
@@ -44,6 +70,7 @@ contract Timelock is Ownable {
 		batch.unlockBlock = 0;
 		batch.canWithdraw = false;
 		IERC20(lockToken).safeTransfer(msg.sender, amount);
-		emit Withdraw(msg.sender, amount);
+		emit Withdraw(msg.sender, amount, _batchId);
 	}
+
 }
