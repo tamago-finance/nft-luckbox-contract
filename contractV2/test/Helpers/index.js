60432c282a319b75e3d4188b41008b5361b67263
pisuthd <pisuth.dae@gmail.com>
2021-11-30 17:24:13 +0700

    cleanup old versions

--- contractV2/test/Helpers/index.js
+++
@@ -1,9 +0,0 @@
-const { ethers } = require("ethers")
-
-exports.fromEther = (value) => {
-    return ethers.utils.formatEther(value)
-}
-
-exports.toEther = (value) => {
-    return ethers.utils.parseEther(`${value}`)
-}
