b7c99ee158983e5d7cfbb1fde2171d4d37957e01
pisuthd <pisuth.dae@gmail.com>
2021-12-01 17:13:17 +0700

    implement the price feeder registry

--- contracts/interfaces/IPriceResolver.sol
+++ contracts/interfaces/IPriceResolver.sol
@@ -4,18 +4,6 @@ pragma solidity 0.6.12;
 
 interface IPriceResolver {
 
-    // function getCurrentPrice() external view returns (uint256);
-
-    // function getCurrentPriceCollateral() external view returns (uint256);
-
-    // function getCurrentRatio() external view returns (uint256);
-
-    // function getRawRatio() external view returns (uint256);
-
-    // function getAvg30Price() external view returns (uint256);
-
-    // function getAvg60Price() external view returns (uint256);
-
-    // function isBullMarket() external view returns (bool);
+    function getCurrentPrice(bytes32 _symbol) external view returns (uint256);
 
 }
