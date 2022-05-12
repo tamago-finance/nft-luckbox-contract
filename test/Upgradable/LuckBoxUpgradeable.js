ab319c09c8e8d136d511cf93b76d33a757ac5a2d
Kusuma Jongpattana <kusumaj@Kusumas-MacBook-Pro.local>
2022-05-12 19:33:18 +0700

    add erc20 and setEvent

--- test/Upgradable/LuckBoxUpgradeable.js
+++ test/Upgradable/LuckBoxUpgradeable.js
@@ -20,6 +20,8 @@ describe("LuckBox V2 Upgradeable - polygon", () => {
       "LuckBoxUpgradeable"
     );
     const MockERC1155 = await ethers.getContractFactory("MockERC1155");
+    const MockERC721 = await ethers.getContractFactory("MockERC721");
+    const MockERC20 = await ethers.getContractFactory("MockERC20");
     const LINK_TOKEN = "0xb0897686c545045aFc77CF20eC7A532E3120E0F1"; // POLYGON LINK
     const VRF_COORDINATOR = "0x3d2341ADb2D31f1c5530cDC622016af293177AE0"; //POLYGON VRF COORDINATOR
     const KEY_HASH =
@@ -29,6 +31,9 @@ describe("LuckBox V2 Upgradeable - polygon", () => {
     erc1155 = await MockERC1155.deploy(
       "https://api.cryptokitties.co/kitties/{id}"
     );
+    erc721 = await MockERC721.deploy("Mock NFT", "MOCK");
+    erc20 = await MockERC20.deploy("Mock Token", "MOCK", 18);
+
     luckBox = await upgrades.deployProxy(LuckBoxUpgradeable, [
       LINK_TOKEN,
       VRF_COORDINATOR,
@@ -167,10 +172,9 @@ describe("LuckBox V2 Upgradeable - polygon", () => {
 
     const newPoapId = [4, 5, 6];
     await luckBox.updatePoaps(eventId, newPoapId);
+    // console.log(await luckBox.events(1));
 
-    console.log(await luckBox.events(1).poaps);
-    // expect((await luckBox.events(1)).name).to.equals("test12345");
-    expect((await luckBox.events(1)).poaps).to.equals(newPoapId);
+    // expect((await luckBox.events(1)).poaps).to.equals(newPoapId);
   });
 
   it("Should emergencyWithdrawERC1155 successfully", async () => {
@@ -183,9 +187,78 @@ describe("LuckBox V2 Upgradeable - polygon", () => {
       await luckBox.createPoap(id, erc1155.address, id, true);
     }
 
-    await emergencyWithdrawERC1155(admin.address, erc1155.address, 1, 1);
+    await erc1155.setApprovalForAll(luckBox.address, true);
 
-    //admin => tokenid 1 => 2 piece
-    // expect(await)
+    await luckBox.depositERC1155(erc1155.address, 1, 1);
+
+    await luckBox.emergencyWithdrawERC1155(
+      admin.address,
+      erc1155.address,
+      1,
+      1
+    );
+
+    expect(await erc1155.balanceOf(admin.address, 1)).to.equals(2);
   });
+
+  it("Should error in emergencyWithdrawERC1155", async () => {
+    // Mint 3 NFT, 2x each
+    const tokenIds = [1, 2, 3];
+
+    for (let id of tokenIds) {
+      await erc1155.mint(admin.address, id, 2, "0x00");
+    }
+
+    await expect(
+      luckBox.emergencyWithdrawERC1155(admin.address, erc1155.address, 1, 1)
+    ).to.revertedWith("ERC1155: insufficient balance for transfer");
+  });
+
+  it("Deposit & Withdraw ERC-721 NFTs to all slots", async () => {
+    const tokenIds = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+    // minting ERC-721
+    for (let id of tokenIds) {
+      await erc721.mint(admin.address, id);
+    }
+
+    await erc721.setApprovalForAll(luckBox.address, true);
+
+    // depositing
+    for (let id of tokenIds) {
+      await luckBox.depositERC721(erc721.address, id);
+    }
+
+    expect(await erc721.balanceOf(admin.address)).to.equal(0);
+    expect(await erc721.balanceOf(luckBox.address)).to.equal(9);
+
+    // withdrawing
+    for (let id of tokenIds) {
+      await luckBox.emergencyWithdrawERC721(admin.address, erc721.address, id);
+    }
+
+    expect(await erc721.balanceOf(admin.address)).to.equal(9);
+    expect(await erc721.balanceOf(luckBox.address)).to.equal(0);
+  });
+
+  // it("Deposit & Withdraw ERC-20 to all slots", async () => {
+  //   erc20.transfer(luckBox.address, 10);
+  // const num = await erc20.balanceOf(admin.address);
+  // console.log(num);
+  // expect(await erc20.balanceOf(admin.address)).to.equal(0);
+  // expect(await erc20.balanceOf(luckBox.address)).to.equal(10);
+  // });
+
+  // it("Should set end event successfully", async () => {
+  //   await luckBox.createPoap(1, erc1155.address, 123, true);
+  //   await luckBox.createPoap(2, erc1155.address, 231, true);
+  //   await luckBox.createPoap(3, erc1155.address, 321, true);
+
+  //   const eventId = 1;
+  //   await luckBox.createEvent(eventId, "test12345", [1, 2, 3]);
+
+  //   await luckBox.setEndEvent(1, true);
+  //   console.log(luckBox.events(1));
+  //   // expect()
+  // });
 });
